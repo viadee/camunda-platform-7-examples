a1b628791e69698621b49984cca475b9dbf960a8
rnschk <rnschk@gmail.com>
2022-05-20 18:00:57 +0200

    docs

--- external-task/external-task-handler-json-variables/src/main/java/de/viadee/bpm/camunda/model/JsonDataType.java
+++ external-task/external-task-handler-json-variables/src/main/java/de/viadee/bpm/camunda/model/JsonDataType.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public interface JsonDataType {
@@ -11,6 +12,14 @@ public interface JsonDataType {
     JsonMapper mapper = new JsonMapper();
 
 
+    /**
+     * Deserialize a given json-string to a given type using a {@link JsonMapper}
+     *
+     * @param json Object of type, serialized as json
+     * @param type Type of object
+     * @return Deserialized object
+     * @param <T> Type used for deserialization
+     */
     static <T> T from(final String json, final Class<T> type) {
         try {
             return mapper.readValue(json, type);
@@ -21,16 +30,49 @@ public interface JsonDataType {
     }
 
 
+    /**
+     * Deserialize a list of given json-strings to a @{@link List} of
+     * objects for a given type using a {@link JsonMapper}
+     *
+     * @param jsons List of json-strings
+     * @param type Type of objects
+     * @return List of deserialized objects
+     * @param <T> Type used for deserialization
+     */
     static <T> List<T> fromList(final List<String> jsons, final Class<T> type) {
         return jsons.stream().map(elem -> JsonDataType.from(elem, type)).collect(Collectors.toList());
     }
 
 
+    /**
+     * Serialize a list of @{@link JsonDataType}-objects to a list of Strings (formatted as json each)
+     *
+     * @param objects Objects to serialize
+     * @return @{@link List} of serialized objects
+     */
     static List<String> toJsonList(final List<? extends JsonDataType> objects) {
-        return objects.stream().map(JsonDataType::toJson).collect(Collectors.toList());
+        return objects.stream()
+                      .filter(Objects::nonNull)
+                      .map(JsonDataType::toJson).collect(Collectors.toList());
     }
 
 
+    /**
+     * Serialize a list of @{@link JsonDataType}-objects to a true json-list
+     *
+     * @param objects Objects to serialize
+     * @return Json-list as String
+     */
+    static String toJsonString(final List<? extends JsonDataType> objects) {
+        return "[" + String.join(",", toJsonList(objects)) + "]";
+    }
+
+
+    /**
+     * Serialize a @{@link JsonDataType}-object to a json-string
+     *
+     * @return Json-representation of this as string
+     */
     default String toJson() {
         try {
             return mapper.writeValueAsString(this);
--- external-task/external-task-handler-json-variables/src/main/java/de/viadee/bpm/camunda/processcontext/AbsProcessContext.java
+++ external-task/external-task-handler-json-variables/src/main/java/de/viadee/bpm/camunda/processcontext/AbsProcessContext.java
@@ -17,6 +17,14 @@ public abstract class AbsProcessContext {
     }
 
 
+    /**
+     * Read an object from process-context stored as json,
+     * deserialize and return as given type.
+     *
+     * @param variableName Name of variable in process-context
+     * @param type Type of object
+     * @return Deserialized object
+     */
     <T extends JsonDataType> T readObject(final String variableName, final Class<T> type) {
         String json = this.readVariable(variableName);
         if (nonNull(json)) {
@@ -27,6 +35,15 @@ public abstract class AbsProcessContext {
         }
     }
 
+
+    /**
+     * Read a list of objects from process-context stored as @{@link List} of strings,
+     * deserialize each and return as @{@link List} of the given type.
+     *
+     * @param variableName Name of variable in process-context
+     * @param type Type of objects
+     * @return Deserialized list of objects
+     */
     <T extends JsonDataType> List<T> readList(final String variableName, final Class<T> type) {
         List<String> jsons = this.readVariable(variableName);
         if (nonNull(jsons)) {
@@ -38,7 +55,7 @@ public abstract class AbsProcessContext {
     }
 
 
-    <T> T readVariable(final String variableName) {
+    private <T> T readVariable(final String variableName) {
         if (this.variables.containsKey(variableName)) {
             return (T) this.variables.get(variableName);
 
